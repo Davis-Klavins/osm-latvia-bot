e7600fa25761b6493e1ed80b68268216b379e5c5
Dāvis Kļaviņš <68686450+Davis-Klavins@users.noreply.github.com>
2022-07-08 19:23:37 +0300

    Update osm_3.sh
    New changeset for every 10000 elements.

--- osm_3.sh
+++ osm_3.sh
@@ -12,8 +12,8 @@ cd $DIRECTORY
 # Create comment file.
 echo "Updated addresses in Latvia." > latvia-diff.comment
 
-# Split OsmChange file in pieces no larger than 5000 elements.
-python split.py "$input.osc" 5000 || exit -1
+# Split OsmChange file in pieces no larger than 10000 elements (changeset limit).
+python split.py "$input.osc" 10000 || exit -1
 
 # Delete bounds element from the 1st OsmChange file.
 xml ed -d '//osmChange/modify/bounds' latvia-diff-part1.osc > latvia-diff-part1-edited.osc
@@ -22,22 +22,15 @@ mv latvia-diff-part1-edited.osc latvia-diff-part1.osc
 # Number of parts as count of *-part*.osc files (https://askubuntu.com/a/454568).
 parts=`find -maxdepth 1 -type f -name "*-part*.osc" -printf x | wc -c`
 
-# Open a new changeset and exit without uploading anything.
-chgset=`python upload.py $ident -c yes -y "Valsts adrešu reģistra informācijas sistēmas atvērtie dati" -n "$input.osc"`
-[ -z "$chgset" ] && exit -1
-
-# Upload changes.
+# Upload changes. New changeset for every 10000 elements.
 for num in `seq 1 $parts`; do
-        python upload.py $ident -c yes "$input-part$num.osc" -s $chgset || exit -1
+        python upload.py $ident -c yes -y "Valsts adrešu reģistra informācijas sistēmas atvērtie dati" "$input-part$num.osc" || exit -1
 
         for rnum in `seq $num $parts`; do
                 path/to/python2/python diffpatch.py "$input-part$num.diff.xml" "$input-part$rnum.osc" || exit -1
                 mv "$input-part$rnum.osc.diffed" "$input-part$rnum.osc" || exit -1
         done
 done
-
-# Close changeset.
-#python close.py $chgset
 
 # Delete OsmChange and comment files.
 #rm *.osc
