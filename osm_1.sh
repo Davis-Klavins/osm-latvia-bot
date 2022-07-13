1d60e6b29160ca23b1e7301ee17318c272dae8c5
Dāvis Kļaviņš <68686450+Davis-Klavins@users.noreply.github.com>
2022-07-13 22:27:34 +0300

    Update osm_1.sh
    Apply changes made after Geofabrik extract has been created.

--- osm_1.sh
+++ osm_1.sh
@@ -20,3 +20,7 @@ rm tags_4_addresses.csv
 py oauth_cookie_client.py -o cookie.txt -s settings.json
 # Workaround from https://pavie.info/2020/08/12/complete-full-history-osm/ because command "wget -O latvia-latest-internal.osm.pbf --load-cookies cookie.txt --max-redirect 0 https://osm-internal.download.geofabrik.de/europe/latvia-latest-internal.osm.pbf" returns "ERROR 403: Forbidden".
 wget -q -O latvia-latest-internal.osm.pbf -N --no-cookies --header "Cookie: $(cat cookie.txt | cut -d ';' -f 1)" https://osm-internal.download.geofabrik.de/europe/latvia-latest-internal.osm.pbf
+
+# Apply changes made after Geofabrik extract has been created.
+osmupdate latvia-latest-internal.osm.pbf latvia-latest-internal-updated.osm.pbf --base-url=http://download.openstreetmap.fr/replication/europe/minute/ -B=latvia.poly
+mv latvia-latest-internal-updated.osm.pbf latvia-latest-internal.osm.pbf
