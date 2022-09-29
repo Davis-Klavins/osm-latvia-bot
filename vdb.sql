58efaec4b8ef1b99c3550787ee004d363dcf04bb
Dāvis Kļaviņš <68686450+Davis-Klavins@users.noreply.github.com>
2022-09-29 16:53:55 +0300

    Update vdb.sql
    Added comments of fields (in Latvian).

--- vdb.sql
+++ vdb.sql
@@ -75,6 +75,46 @@ FROM lgia.vdb_orig;
 
 CREATE INDEX vdb_geom_idx ON lgia.vdb USING GIST (geom);
 
+COMMENT ON TABLE lgia.vdb IS 'LĢIA Vietvārdu datubāze';
+
+COMMENT ON COLUMN lgia.vdb.objektaid IS 'ID';
+
+COMMENT ON COLUMN lgia.vdb.pamatnosaukums IS 'Nosaukums, kas no visiem datubāzē uzkrātajiem ir atzīts par lietošanai vai oficiālai apstiprināšanai (t.sk. neveiksmīgi apstiprināta nosaukuma labošanai) vispiemērotāko atbilstoši Ministru kabineta 10.01.2012. noteikumu Nr. 50 "Vietvārdu informācijas noteikumi" prasībām.';
+
+COMMENT ON COLUMN lgia.vdb.pamatnosaukums2 IS 'Paralēlais pamatnosaukums (ūdenstecēm var būt arī atsevišķa tās posma nosaukums)';
+
+COMMENT ON COLUMN lgia.vdb.objekta_veids IS 'Objekta veids';
+
+COMMENT ON COLUMN lgia.vdb.stavoklis IS 'Stāvoklis';
+
+COMMENT ON COLUMN lgia.vdb.oficialais_nosaukums IS 'Oficiālais nosaukums';
+
+COMMENT ON COLUMN lgia.vdb.oficialais_nosaukums_ar IS 'Oficiālais nosaukums Adrešu reģistrā';
+
+COMMENT ON COLUMN lgia.vdb.citi_nosaukumi IS 'Citi nosaukumi un nosaukumu varianti, nenorādot, kuri ir kļūdaini vai novecojuši.';
+
+COMMENT ON COLUMN lgia.vdb.galv_pagasts IS 'Galvenā pagasta nosaukums';
+
+COMMENT ON COLUMN lgia.vdb.galv_novads IS 'Galvenā novada nosaukums';
+
+COMMENT ON COLUMN lgia.vdb.galv_rajons_agrak IS 'Galvenā rajona nosaukums';
+
+COMMENT ON COLUMN lgia.vdb.atkkods IS 'Administratīvi teritoriālās vienības kods (ATVK)';
+
+COMMENT ON COLUMN lgia.vdb.arisid IS 'Adresācijas objekta kods Adrešu reģistrā';
+
+COMMENT ON COLUMN lgia.vdb.raksturojums IS 'Raksturojums';
+
+COMMENT ON COLUMN lgia.vdb.zinas_par_objektu IS 'Ziņas par objektu';
+
+COMMENT ON COLUMN lgia.vdb.papildus_zinas_par_nosaukumu IS 'Papildus ziņas par nosaukumu';
+
+COMMENT ON COLUMN lgia.vdb.geom IS 'Ģeometrija (punktveida objektu atrašanās vietas, laukumveida objektu aptuvenie centri un ūdensteču ietekas ūdenstilpēs vai citās ūdenstecēs)';
+
+COMMENT ON COLUMN lgia.vdb.izveides_datums IS 'Izveides datums Vietvārdu datubāzē';
+
+COMMENT ON COLUMN lgia.vdb.pedejo_izmainu_datums IS 'Pēdējo izmaiņu datums Vietvārdu datubāzē';
+
 END
 $$ LANGUAGE plpgsql;
 
