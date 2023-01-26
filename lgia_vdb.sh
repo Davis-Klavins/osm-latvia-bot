bb50a5c43443f4765df86e04be0a22a61e2db8b6
Dāvis Kļaviņš <68686450+Davis-Klavins@users.noreply.github.com>
2023-01-26 22:02:47 +0200

    Update lgia_vdb.sh
    Changed to English page (file temporarily not available in Latvian), shortened command.

--- lgia_vdb.sh
+++ lgia_vdb.sh
@@ -12,9 +12,7 @@ cd $DIRECTORY
 
 # Download the Place Names Database of the Latvian Geospatial Information Agency.
 cd lgia
-wget -q -O index.html https://www.lgia.gov.lv/lv/place-names-data-open-data
 # https://stackoverflow.com/a/11826500
-cat index.html | grep -o '<a href="https://s3.storage.pub.lvdc.gov.lv/lgia-opendata/citi/vdb/.*.xlsx".*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | xargs wget -q -O VDB_OBJEKTI.xlsx
-rm index.html
+wget -q -O - https://www.lgia.gov.lv/en/place-name-data-open-data | grep -o '<a href="https://s3.storage.pub.lvdc.gov.lv/lgia-opendata/citi/vdb/.*.xlsx".*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | xargs wget -q -O VDB_OBJEKTI.xlsx
 
 psql -h $IP_ADDRESS -p $PORT -U osm -d osm -w -c "CALL lgia.vdb()"
